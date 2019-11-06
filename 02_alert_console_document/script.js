84747075ffd1b416ff30646a1b94fbe0f2e1d23d
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2019-11-06 20:11:15 +0100

    Init alert console document

---
+++ 02_alert_console_document/script.js
@@ -0,0 +1,5 @@
+//alert('Hello world');
+//console.log('Hello world');
+//console.log(document);
+//document.write('Hello world via write method');
+document.getElementById('mydiv').innerHTML = 'Hello world via innertHTML';
